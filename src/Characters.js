6a7eaa09551fbb73154558691a86ff4daf6aa248
MrGanesh <ganeshkulkarni689@gmail.com>
2022-07-04 13:29:50 +0530

    1 file modified

--- src/Characters.js
+++ src/Characters.js
@@ -1,1 +1,27 @@
-import 
+import React from "react"
+import {useQuery, gql} from "@apollo/client"
+const GET_CHARACTER = gql`
+query GetCharacters($id: ID!){
+  character(id:$id){
+    name
+    id
+    image
+    episode{
+      name
+      episode
+    }
+  }
+}
+`
+export default function Characters(){
+  const {data, loading, error} = useQuery(GET_CHARACTER,{
+    variables:{
+      id:4
+    }
+  })
+
+  return(
+    <div>
+      </div>
+  )
+}
